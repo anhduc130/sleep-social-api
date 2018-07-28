392a40e7c54e48f622413d114865c3a64aafa9c3
Andrew Bui <andrewbui130@hotmail.com>
2018-07-28 15:41:33 -0600

    Update sign-up.js
    Delete access key

--- sign-up.js
+++ sign-up.js
@@ -6,7 +6,7 @@ module.exports = (username, password, key) => {
         .post(process.env.POST_FIREBASE_DB_URL)
         .send({ username, password, accessToken: uuid.v4() })
         .set("Content-Type", "application/json")
-        .set("Authorization", )  // I understand this is super bad :P But time is running out
+        .set("Authorization", key)  // I understand this is super bad :P But time is running out
         .end((res) => {
             if (!res) {
                 return false;
