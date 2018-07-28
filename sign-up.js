fe39fb345ca1b8c43537ffc86c4d6ca2a587ce74
Andrew Bui <andrewbui130@hotmail.com>
2018-07-28 12:40:06 -0600

    Update sign-up.js
    Add a comment

--- sign-up.js
+++ sign-up.js
@@ -6,7 +6,7 @@ module.exports = (username, password) => {
         .post(process.env.POST_FIREBASE_DB_URL)
         .send({ username, password, accessToken: uuid.v4() })
         .set("Content-Type", "application/json")
-        .set("Authorization", "key=AIzaSyAGy1KOYH6gAkyD1E4iHGkq4eaj7uf7lr0")
+        .set("Authorization", "key=AIzaSyAGy1KOYH6gAkyD1E4iHGkq4eaj7uf7lr0")  // I understand this is super bad :P But time is running out
         .end((res) => {
             if (!res) {
                 return false;
