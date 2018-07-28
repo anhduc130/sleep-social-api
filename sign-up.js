4f3d1b9ea78c2be00f93b052e77800369ea29b40
Andrew Bui <andrewbui130@hotmail.com>
2018-07-28 15:41:07 -0600

    Update sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -1,12 +1,12 @@
 const request = require("superagent");
 const uuid = require("uuid");
 
-module.exports = (username, password) => {
+module.exports = (username, password, key) => {
     return request
         .post(process.env.POST_FIREBASE_DB_URL)
         .send({ username, password, accessToken: uuid.v4() })
         .set("Content-Type", "application/json")
-        .set("Authorization", "key=AIzaSyAGy1KOYH6gAkyD1E4iHGkq4eaj7uf7lr0")  // I understand this is super bad :P But time is running out
+        .set("Authorization", )  // I understand this is super bad :P But time is running out
         .end((res) => {
             if (!res) {
                 return false;
